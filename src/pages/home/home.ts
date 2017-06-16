1f11cf2319df56def674ec035f271f842e2a8f32
Victor Ojeda <ssvictoro@master496.salud.gob.ar>
2017-06-16 14:21:22 -0300

    Inicio proyecto

---
+++ src/pages/home/home.ts
@@ -0,0 +1,84 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Observable } from 'rxjs/Rx';
+ 
+declare var google;
+let i = 0; 
+@Component({
+  selector: 'home-page',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  subscription;
+    
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+ 
+  }
+ 
+  ionViewDidLoad(){
+      this.subscription = Observable.interval(10000).subscribe(x => {
+      // the number 1000 is on miliseconds so every second is going to have an iteration of what is inside this code.
+        console.log (i);
+        i++;
+      });    
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    this.geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 20,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      let marker = new google.maps.Marker({
+      map: this.map,
+      icon: {
+      path: google.maps.SymbolPath.CIRCLE,
+      strokeWeight:5,
+      strokeColor:"#5882FA",
+      fillColor:"#81BEF7",
+      fillOpacity: 1,
+      scale: 9,
+      },
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+      });
+ 
+      let content = "<h4>Information!</h4>";          
+ 
+      this.addInfoWindow(marker, content);
+ 
+    }, (err) => {
+      console.log(err);
+    });
+ 
+  }
+
+  function(success){
+    console.log(success);
+  }
+
+ addInfoWindow(marker, content){
+ 
+  let infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+ 
+  google.maps.event.addListener(marker, 'click', () => {
+    infoWindow.open(this.map, marker);
+  });
+ 
+ } 
+}
